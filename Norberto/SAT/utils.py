aef6a91e77ba94e8d07cc5ea50504997d8ec112a
BandoleroNext <norbycasa@gmail.com>
2024-11-04 17:03:54 +0100

    Utlis created for SAT

---
+++ Norberto/SAT/utils.py
@@ -0,0 +1,33 @@
+from itertools import combinations
+from z3 import *
+
+
+#Naive pairwise constraints, needed for the sequential version
+def at_least_one_np(bool_vars):
+    return Or(bool_vars)
+
+def at_most_one_np(bool_vars, name = ""):
+    return And([Not(And(pair[0], pair[1])) for pair in combinations(bool_vars, 2)])
+
+def exactly_one_np(bool_vars, name = ""):
+    return And(at_least_one_np(bool_vars), at_most_one_np(bool_vars, name))
+
+
+#Sequential pairwise constraints, higher efficiency
+def at_least_one_seq(bool_vars):
+    return at_least_one_np(bool_vars)
+
+def at_most_one_seq(bool_vars, name):
+    constraints = []
+    n = len(bool_vars)
+    s = [Bool(f"s_{name}_{i}") for i in range(n - 1)]
+    constraints.append(Or(Not(bool_vars[0]), s[0]))
+    constraints.append(Or(Not(bool_vars[n-1]), Not(s[n-2])))
+    for i in range(1, n - 1):
+        constraints.append(Or(Not(bool_vars[i]), s[i]))
+        constraints.append(Or(Not(bool_vars[i]), Not(s[i-1])))
+        constraints.append(Or(Not(s[i-1]), s[i]))
+    return And(constraints)
+
+def exactly_one_seq(bool_vars, name):
+    return And(at_least_one_seq(bool_vars), at_most_one_seq(bool_vars, name))
