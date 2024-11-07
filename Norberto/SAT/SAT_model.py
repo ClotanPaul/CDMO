ffa721d07103c5173c8e92604b66daca79504364
BandoleroNext <norbycasa@gmail.com>
2024-11-07 14:10:13 +0100

    Sat Model finds a solution (need work on constraint and best solution tree)

--- Norberto/SAT/SAT_model.py
+++ Norberto/SAT/SAT_model.py
@@ -1,21 +1,44 @@
-import numpy as np
 from z3 import *
+import re
 
-from .utils import *
+from utils import *
+
+time = 4
+
+def data_parsing(data):
+    text = data.split("\n")
+    num_reg = r"(\d+)"
+    text = [x for x in text if x != ""]
+    text = [x.strip() for x in text]
+    m = int(re.findall(num_reg, text[0])[0])
+    n = int(re.findall(num_reg, text[1])[0])
+    l = [int(x) for x in re.findall(num_reg, text[2])]
+    s = [int(x) for x in re.findall(num_reg, text[3])]
+
+    D = []
+    for i in range(4, 4 + n + 1):
+        D.append([int(x) for x in re.findall(num_reg, text[i])])
+
+    return {
+        "m": m,
+        "n": n,
+        "l": l,
+        "s": s,
+        "D": D,
+    }
 
 
-def mcp_pb(instance):
-    m = instance["m"] # courriers
+
+def mcp(instance):
+    m = instance["m"] # couriers
     n = instance["n"] # packages
     l = instance["l"] # weigths
-    s = instance["s"] # sizes of courriers
+    s = instance["s"] # sizes of couriers
     time = 4
-    min_load = instance["min_load"] # min load of each courrier
-    max_load = instance["max_load"] # max load of each courrier
 
     solver = Solver()
 
-    # To codify that courrier i deliver package j at time k
+    # To codify that courier i deliver package j at time k
     v = [[[Bool(f"x_{i}_{j}_{k}") for k in range(time+1)] for j in range (n+1)] for i in range(m)]
 
     d = [[[Bool(f"d_{i}_{start}_{end}") for end in range(n+1)]
@@ -29,13 +52,15 @@ def mcp_pb(instance):
                         Implies(And(v[i][startj][k], v[i][endj][k+1]), d[i][startj][endj])
                     )
 
+
     # Constraints
+    """
     # 1. Each courier can carry at most l[i] kg
     # Pb version
     for i in range(m):
        solver.append(PbLe([(v[i][j][k],s[j]) for j in range(n) for k in range(1,time)], min(l[i], max_load)))
        solver.append(PbGe([(v[i][j][k],s[j]) for j in range(n) for k in range(1,time)], min_load))
-
+    """
 
     # 2. Each courier i starts and ends at position j = n
     for i in range(m):
@@ -54,3 +79,20 @@ def mcp_pb(instance):
         solver.add(exactly_one_seq([v[i][j][k] for k in range(1,time) for i in range(m)], f"exactly_once_{j}")) #(PbEq([(v[i][j][k],1) for k in range(1,n+1) for i in range(m)],1))    
 
     return solver, v, d
+
+
+def format_solution(m, n, time, model, v):
+    solution = []
+
+    for i in range(m):
+        courier_solution = []
+
+        for k in range(time):
+            for j in range(n):
+                if model[v[i][j][k]]:
+                    courier_solution.append(j + 1)
+
+        solution.append(courier_solution)
+    return solution
+
+
